3ed9048350e4e95f1ab80fb91f3ae178e324dc47
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-11-14 17:39:31 +0100

    migrate

--- database/migrations/2022_11_14_162821_create_posts_table.php
+++ database/migrations/2022_11_14_162821_create_posts_table.php
@@ -15,6 +15,9 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
