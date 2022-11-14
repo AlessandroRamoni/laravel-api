72a060cc5cfc13e5f8201380baffe8f7e564c79c
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-11-14 15:37:32 +0100

    rename app.name

---
+++ resources/views/guests/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=7, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name', 'Front Office') }}</title>
+
+
+</head>
+
+<body>
+    <h1>Sono nella home guest</h1>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
 
 Auth::routes();
 
@@ -29,3 +32,7 @@ Route::middleware('auth')
         Route::get('/', 'HomeController@index')
             ->name('home');
     });
+
+Route::get("{any?}", function(){
+    return view("guests.home");
+})->where("any", ".*");
