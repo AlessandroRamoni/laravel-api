dcad5cbf136c770f6c120aeee73c0fdc9de91cf7
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-11-14 19:25:20 +0100

    add create.blade

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    @if ($errors->any())
+        <h2>Form errors: please fill all spaces correctly!</h2>
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <form method="POST" action="{{ route('admin.posts.store') }}">
+        @csrf
+        <div>
+            <label for="title">Title:</label>
+            <input required maxlength="255" type="text" name="title" value="{{ old('title') }}">
+            @error('title')
+                <div class="my-2 bg-danger text-white">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+        <div>
+            <label for="content">Content:</label>
+            <textarea required name="content" cols="30" rows="10">{{ old('content') }}</textarea>
+            @error('content')
+                <div class="my-2 bg-danger text-white">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+        <input type="submit" value="Create">
+    </form>
+    <div class="mt-5">
+        <a href="{{ route('admin.posts.index') }}">BACK TO THE POSTS LIST</a>
+    </div>
+@endsection
