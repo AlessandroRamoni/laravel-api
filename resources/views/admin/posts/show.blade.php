878d683b54ca303d6d34a464478d40a5db65676b
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-11-14 21:39:08 +0100

    add form delete

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -7,4 +7,10 @@
     <div class="mt-5">
         <a href="{{ route('admin.posts.edit', $post->id) }}">Edit</a>
     </div>
+
+    <form action="{{ route('admin.posts.destroy', ['post' => $post->id]) }}" method="POST">
+        @csrf
+        @method('DELETE')
+        <input type="submit" class="delete-btn" value="DELETE">
+    </form>
 @endsection
