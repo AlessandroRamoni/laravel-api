e1c50b904c33fcc60900cb5feecba370d9b8631d
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-11-14 18:25:23 +0100

    create ciclo for in PostSeeder

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Database\Seeder;
 
+use Faker\Generator as Faker;
+
+use App\Post;
+
+use Illuminate\Support\Str;
+
 class PostSeeder extends Seeder
 {
     /**
@@ -9,8 +15,28 @@ class PostSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
         //
+        for ($i = 0; $i < 20; $i++) {
+            $post = new Post();
+            $post->title = $faker->realText($maxNbChars = 200, $indexSize = 2);
+            $post->content = $faker->text();
+
+            $slug = Str::slug($post->title);
+            $slug_base = $slug;
+            $counter = 1;
+            $existingPost = Post::where('slug', $slug)->first();
+            while ($existingPost) {
+                $slug = $slug_base . '_' . $counter;
+                $existingPost = Post::where('slug', $slug)->first();
+                $counter++;
+            }
+
+            $post->slug = $slug;
+
+            $post->save();
+        }
     }
+
 }
