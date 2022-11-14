183585663ba854d6996d370a4f891ffbc8406913
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-11-14 17:05:30 +0100

    edit webpack.mix.js

---
+++ resources/js/views/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <ExampleComponent />
+    </div>
+</template>
+
+<script>
+import ExampleComponent from '../components/ExampleComponent.vue';
+
+export default {
+    name: 'App',
+    components: [ExampleComponent]
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
