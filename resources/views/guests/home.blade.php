75bff4e32a797428b80178e28a03c36a0bf1214d
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-11-14 15:51:32 +0100

    edit font

--- resources/views/guests/home.blade.php
+++ resources/views/guests/home.blade.php
@@ -7,6 +7,10 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Front Office') }}</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Luckiest+Guy&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
 
 </head>
