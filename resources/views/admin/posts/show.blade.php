a8bf2844b38ec40135de6934938ef3bdbaa42b88
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-11-14 19:25:39 +0100

    add show.blade

---
+++ resources/views/admin/posts/show.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <h1>{{ $post->title }}</h1>
+    <p>{{ $post->content }}</p>
+
+    <div class="mt-5">
+        <a href="{{ route('admin.posts.edit') }}">Edit</a>
+    </div>
+@endsection
