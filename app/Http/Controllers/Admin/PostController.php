acc61b696b5ccfb4e302da608ddb39abd8cbf01b
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-11-14 19:55:02 +0100

    add  public function update

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -93,6 +93,20 @@ class PostController extends Controller
     public function update(Request $request, Post $post)
     {
         //
+        $request->validate([
+            'title' => 'required|min:5|max:255',
+            'content' => 'required',
+        ]);
+        $form_data = $request->all();
+
+        if ($post->title != $form_data['title']) {
+            $slug = $this->getSlug($form_data['title']);
+            $form_data['slug'] = $slug;
+        }
+
+        $post->update($form_data);
+
+        return redirect()->route('admin.posts.show', $post->id);
     }
 
     /**
