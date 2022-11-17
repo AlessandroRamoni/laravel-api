192111f7df827b20d4c5fa84644dcd6eb75f7b7c
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-11-17 18:32:50 +0100

    add link

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -2,6 +2,11 @@
 
 @section('content')
     <h2>{{ $category->name }}</h2>
+
+    <div class="mb-2">
+        <a href="{{ route('admin.categories.edit', $category->id) }}">Modifica</a>
+    </div>
+
     @foreach ($category->posts as $post)
         <div>
             <a href="{{ route('admin.posts.show', $post->id) }}"> {{ $post->title }}</a>
