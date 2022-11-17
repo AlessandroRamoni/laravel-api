698be3055d8be1cd18111e665b141f6dba98da09
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-11-17 18:49:59 +0100

    add update

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -96,7 +96,20 @@ class CategoryController extends Controller
     public function update(Request $request, Category $category)
     {
         //
-        dd($category);
+        // dd($category);
+
+        $request->validate([
+            'name' => 'required|max:255'
+        ]);
+
+        $form_data = $request->all();
+        if($category->name != $form_data['name']){
+            $slug = $this->getSlug($form_data['name']);
+            $form_data['slug'] = $slug;
+        }
+        $category->update($form_data);
+
+        return redirect()->route('admin.categories.show', $category->id);
     }
 
     /**
