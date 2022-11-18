71d189a453271a2d88a832f6253607c6d848a814
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-11-18 18:38:17 +0100

    add show.blade in tag folder

---
+++ resources/views/admin/tags/show.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <h1>{{ $tag->name }}</h1>
+
+
+    <div @error('name') class="is-invalid" @enderror>
+        <form action="{{ route('admin.tags.update', $tag->id) }}" method="POST">
+            @csrf
+            @method('PUT')
+            <input type="text" name="name" value="{{ old('name', $tag->name) }}">
+            @error('name')
+                <div>
+                    {{ $message }}
+                </div>
+            @enderror
+            <input type="submit" value="Aggiorna">
+        </form>
+    </div>
+
+    <div class="my-2">
+        <form action="{{ route('admin.tags.destroy', $tag->id) }}" method="POST">
+            @csrf
+            @method('DELETE')
+            <input type="submit" value="DELETE">
+        </form>
+    </div>
+@endsection
