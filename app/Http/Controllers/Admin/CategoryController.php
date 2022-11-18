1c9106a254c0da03402a8f174cb7874fb1df1520
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-11-18 08:38:23 +0100

    edit categoryController delete

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -118,9 +118,11 @@ class CategoryController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Category $category)
     {
         //
+        $category->delete();
+        return redirect()->route('admin.categories.index');
     }
 
     private function getSlug($name){
