621a09b6f5658823e57e2da19b7fbd38c07de794
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-11-18 17:50:43 +0100

    add $tags = Tag::all();

--- app/Http/Controllers/Admin/TagController.php
+++ app/Http/Controllers/Admin/TagController.php
@@ -16,6 +16,8 @@ class TagController extends Controller
     public function index()
     {
         //
+        $tags = Tag::all();
+        return view('admin.tags.index', compact('tags'));
     }
 
     /**
@@ -48,6 +50,7 @@ class TagController extends Controller
     public function show(Tag $tag)
     {
         //
+        dd($tag);
     }
 
     /**
