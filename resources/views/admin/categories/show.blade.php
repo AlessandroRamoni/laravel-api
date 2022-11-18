360bd66cf2088e85398277ea2806d4d1d9a4db78
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-11-18 08:27:05 +0100

    add show.blade

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -7,6 +7,18 @@
         <a href="{{ route('admin.categories.edit', $category->id) }}">Modifica</a>
     </div>
 
+
+    {{-- <div class="mb-2">
+        <a href="{{ route('admin.categories.destroy', $category->id) }}">Elimina</a>
+    </div> --}}
+
+    <form action="{{ route('admin.categories.destroy', $category->id) }}" method="POST">
+        @csrf
+        @method('DELETE')
+
+        <input class="delete-btn" type="submit" value="Cancella" onclick="return confirm('Sei sicuro di volerlo cancellare?')">
+    </form>
+
     @foreach ($category->posts as $post)
         <div>
             <a href="{{ route('admin.posts.show', $post->id) }}"> {{ $post->title }}</a>
