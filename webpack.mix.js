9d4c75b620efe42657416aa5210f8054e125a8fa
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-11-21 16:02:53 +0100

    add route backend

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,5 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/front.js', 'public/js')
+    .js('resources/js/back.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
