cba3b0fddeed9de791579eed6842a66b3e05aeeb
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-11-21 14:04:14 +0100

    composer install - npm i - npm run dev

--- public/js/front.js
+++ public/js/front.js
@@ -49847,8 +49847,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\MAMP\htdocs\laravel-many-to-many-new\resources\js\front.js */"./resources/js/front.js");
-module.exports = __webpack_require__(/*! C:\MAMP\htdocs\laravel-many-to-many-new\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\MAMP\htdocs\laravel-api\resources\js\front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! C:\MAMP\htdocs\laravel-api\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
